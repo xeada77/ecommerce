4c8abf0f6254323fe4263e9bcfc0a9ff41178063
xeada77 <luismartq@gmail.com>
2021-09-24 20:32:39 +0200

    Products components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Annoucement from "../components/Annoucement";
+import Categories from "../components/Categories";
 import Navbar from "../components/Navbar";
+import Products from "../components/Products";
 import Slider from "../components/Slider";
 
 const Home = () => {
@@ -9,6 +11,8 @@ const Home = () => {
       <Annoucement />
       <Navbar />
       <Slider />
+      <Categories />
+      <Products />
     </div>
   );
 };
